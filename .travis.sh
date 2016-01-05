e016d017d080d4b2b6e27c434ffe3e2d5272ba5b
Anil Madhavapeddy <anil@recoil.org>
2016-01-05 14:58:57 +0000

    shift to the new docker remote

--- .travis.sh
+++ .travis.sh
@@ -5,5 +5,5 @@ git clone git://github.com/ocaml/ocaml-travisci-skeleton $OS/__test
 chmod -R a+w $OS
 docker run -v \
   $OS:/repo \
-  ocaml/opam-dockerfiles:${DISTRO}_ocaml-${OCAML_VERSION} \
+  ocaml/opam:${DISTRO}_ocaml-${OCAML_VERSION} \
   /repo/.travis-build.sh
