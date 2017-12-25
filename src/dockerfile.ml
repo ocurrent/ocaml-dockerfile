c5c1547eb2a023efc16467fc2eb3b769399d7996
Anil Madhavapeddy <anil@recoil.org>
2017-12-25 18:46:18 +0000

    format with ocamlformat

--- src/dockerfile.ml
+++ src/dockerfile.ml
@@ -17,18 +17,18 @@
 
 open Sexplib.Conv
 
-type shell_or_exec = [
-  | `Shell of string
-  | `Shells of string list
-  | `Exec of string list
-] [@@deriving sexp]
+type shell_or_exec =
+  [`Shell of string | `Shells of string list | `Exec of string list]
+  [@@deriving sexp]
 
 type sources_to_dest =
-  [`From of string option ] * [ `Src of string list ] * [ `Dst of string ] [@@deriving sexp]
+  [`From of string option] * [`Src of string list] * [`Dst of string]
+  [@@deriving sexp]
 
-type line = [
-  | `Comment of string
-  | `From of [ `Image of string * string option | `Image_tag of string * string * string option ]
+type line =
+  [ `Comment of string
+  | `From of [ `Image of string * string option
+             | `Image_tag of string * string * string option ]
   | `Maintainer of string
   | `Run of shell_or_exec
   | `Cmd of shell_or_exec
@@ -41,13 +41,17 @@ type line = [
   | `User of string
   | `Workdir of string
   | `Onbuild of line
-  | `Label of (string * string) list
-] [@@deriving sexp]
+  | `Label of (string * string) list ]
+  [@@deriving sexp]
 
 type t = line list [@@deriving sexp]
-let (@@) = (@)
-let (@@@) = List.fold_left (fun a b -> a @@ b)
+
+let ( @@ ) = ( @ )
+
+let ( @@@ ) = List.fold_left (fun a b -> a @@ b)
+
 let empty = []
+
 let maybe f = function None -> empty | Some v -> f v
 
 open Printf
@@ -58,27 +62,32 @@ let crunch l =
   let pack l =
     let rec aux acc = function
       | [] -> acc
-      | (`Run (`Shell a)) :: (`Run (`Shell b)) :: tl ->
-         aux ((`Run (`Shells [a;b]))::acc) tl
-      | (`Run (`Shells a)) :: (`Run (`Shell b)) :: tl ->
-         aux ((`Run (`Shells (a @ [b])))::acc) tl
-      | (`Run (`Shells a)) :: (`Run (`Shells b)) :: tl ->
-         aux ((`Run (`Shells (a @ b)))::acc) tl
-      | hd :: tl -> aux (hd::acc) tl in
-    List.rev (aux [] l) in
+      | (`Run `Shell a) :: (`Run `Shell b) :: tl ->
+          aux (`Run (`Shells [a; b]) :: acc) tl
+      | (`Run `Shells a) :: (`Run `Shell b) :: tl ->
+          aux (`Run (`Shells (a @ [b])) :: acc) tl
+      | (`Run `Shells a) :: (`Run `Shells b) :: tl ->
+          aux (`Run (`Shells (a @ b)) :: acc) tl
+      | hd :: tl -> aux (hd :: acc) tl
+    in
+    List.rev (aux [] l)
+  in
   let rec fixp fn l =
     let a = fn l in
-    if a = l then l else fixp fn a in
+    if a = l then l else fixp fn a
+  in
   fixp pack l
-  
-let nl fmt = ksprintf (fun b -> b ^ "\n") fmt
+
+
 let quote s = sprintf "%S" s
+
 let cmd c r = c ^ " " ^ r
 
 let json_array_of_list sl =
   sprintf "[ %s ]" (String.concat ", " (List.map quote sl))
 
-let string_of_shell_or_exec (t:shell_or_exec) =
+
+let string_of_shell_or_exec (t: shell_or_exec) =
   match t with
   | `Shell s -> s
   | `Shells [] -> ""
@@ -86,26 +95,30 @@ let string_of_shell_or_exec (t:shell_or_exec) =
   | `Shells l -> String.concat " && \\\n  " l
   | `Exec sl -> json_array_of_list sl
 
-let string_of_env_list =
-  function
-  | [(k,v)] -> sprintf "%s %s" k v
-  | el -> String.concat " " (List.map (fun (k,v) -> sprintf "%s=%S" k v) el)
 
-let string_of_sources_to_dest (t:sources_to_dest) =
+let string_of_env_list = function
+  | [(k, v)] -> sprintf "%s %s" k v
+  | el -> String.concat " " (List.map (fun (k, v) -> sprintf "%s=%S" k v) el)
+
+
+let string_of_sources_to_dest (t: sources_to_dest) =
   match t with
   | `From None, `Src sl, `Dst d -> String.concat " " (sl @ [d])
-  | `From (Some frm), `Src sl, `Dst d -> sprintf "--from=%s %s" frm (String.concat " " (sl @ [d]))
+  | `From Some frm, `Src sl, `Dst d ->
+      sprintf "--from=%s %s" frm (String.concat " " (sl @ [d]))
+
 
 let string_of_label_list ls =
-  List.map (fun (k,v) -> sprintf "%s=%S" k v) ls |> String.concat " "
+  List.map (fun (k, v) -> sprintf "%s=%S" k v) ls |> String.concat " "
 
-let rec string_of_line (t:line) = 
+
+let rec string_of_line (t: line) =
   match t with
-  | `Comment c -> cmd "#"  c
-  | `From (`Image (i, None)) -> cmd "FROM" i
-  | `From (`Image (i, Some alias)) -> cmd "FROM" (i ^ " as " ^ alias)
-  | `From (`Image_tag (i,t,None)) -> sprintf "FROM %s:%s" i t
-  | `From (`Image_tag (i,t,Some alias)) -> sprintf "FROM %s:%s as %s" i t alias
+  | `Comment c -> cmd "#" c
+  | `From `Image (i, None) -> cmd "FROM" i
+  | `From `Image (i, Some alias) -> cmd "FROM" (i ^ " as " ^ alias)
+  | `From `Image_tag (i, t, None) -> sprintf "FROM %s:%s" i t
+  | `From `Image_tag (i, t, Some alias) -> sprintf "FROM %s:%s as %s" i t alias
   | `Maintainer m -> cmd "MAINTAINER" m
   | `Run c -> cmd "RUN" (string_of_shell_or_exec c)
   | `Cmd c -> cmd "CMD" (string_of_shell_or_exec c)
@@ -120,31 +133,52 @@ let rec string_of_line (t:line) =
   | `Onbuild t -> cmd "ONBUILD" (string_of_line t)
   | `Label ls -> cmd "LABEL" (string_of_label_list ls)
 
+
 (* Function interface *)
 let from ?alias ?tag img =
   match tag with
-  | None -> [ `From (`Image (img, alias)) ]
-  | Some tag -> [ `From (`Image_tag (img, tag, alias)) ]
+  | None -> [`From (`Image (img, alias))]
+  | Some tag -> [`From (`Image_tag (img, tag, alias))]
 
-let comment fmt = ksprintf (fun c -> [ `Comment c ]) fmt
-let maintainer fmt = ksprintf (fun m -> [ `Maintainer m ]) fmt
-let run fmt = ksprintf (fun b -> [ `Run (`Shell b) ]) fmt
-let run_exec cmds : t = [ `Run (`Exec cmds) ]
-let cmd fmt = ksprintf (fun b -> [ `Cmd (`Shell b) ]) fmt
-let cmd_exec cmds : t = [ `Cmd (`Exec cmds) ]
-let expose_port p : t = [ `Expose [p] ]
-let expose_ports p : t = [ `Expose p ]
-let env e : t = [ `Env e ]
-let add ?from ~src ~dst () : t = [ `Add (`From from, `Src src, `Dst dst) ]
-let copy ?from ~src ~dst () : t = [ `Copy (`From from, `Src src, `Dst dst) ]
-let user fmt = ksprintf (fun u -> [ `User u ]) fmt
+
+let comment fmt = ksprintf (fun c -> [`Comment c]) fmt
+
+let maintainer fmt = ksprintf (fun m -> [`Maintainer m]) fmt
+
+let run fmt = ksprintf (fun b -> [`Run (`Shell b)]) fmt
+
+let run_exec cmds : t = [`Run (`Exec cmds)]
+
+let cmd fmt = ksprintf (fun b -> [`Cmd (`Shell b)]) fmt
+
+let cmd_exec cmds : t = [`Cmd (`Exec cmds)]
+
+let expose_port p : t = [`Expose [p]]
+
+let expose_ports p : t = [`Expose p]
+
+let env e : t = [`Env e]
+
+let add ?from ~src ~dst () : t = [`Add (`From from, `Src src, `Dst dst)]
+
+let copy ?from ~src ~dst () : t = [`Copy (`From from, `Src src, `Dst dst)]
+
+let user fmt = ksprintf (fun u -> [`User u]) fmt
+
 let onbuild t = List.map (fun l -> `Onbuild l) t
-let volume fmt = ksprintf (fun v -> [ `Volume [v] ]) fmt
-let volumes v : t = [ `Volume v ]
-let label ls = [ `Label ls ]
-let entrypoint fmt = ksprintf (fun e -> [ `Entrypoint (`Shell e) ]) fmt
-let entrypoint_exec e : t = [ `Entrypoint (`Exec e) ]
-let workdir fmt = ksprintf (fun wd -> [ `Workdir wd ]) fmt
+
+let volume fmt = ksprintf (fun v -> [`Volume [v]]) fmt
+
+let volumes v : t = [`Volume v]
+
+let label ls = [`Label ls]
+
+let entrypoint fmt = ksprintf (fun e -> [`Entrypoint (`Shell e)]) fmt
+
+let entrypoint_exec e : t = [`Entrypoint (`Exec e)]
+
+let workdir fmt = ksprintf (fun wd -> [`Workdir wd]) fmt
 
 let string_of_t tl = String.concat "\n" (List.map string_of_line tl)
+
 let pp ppf tl = Fmt.pf ppf "%s" (string_of_t tl)
