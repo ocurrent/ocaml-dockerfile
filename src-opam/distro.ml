5f0e5335e3f048e7f02c7f36bb2dd1a2fee8ca78
David Allsopp <david.allsopp@metastack.com>
2022-12-17 10:30:42 +0000

    Always use the alternate repo for arm32 and i386
    --platform doesn't appear to be working correctly

--- src-opam/distro.ml
+++ src-opam/distro.ml
@@ -1259,8 +1259,6 @@ let base_distro_tag ?win10_revision ?(arch = `X86_64) d =
         | `Unstable -> "unstable"
       in
       match (arch, v) with
-      | `I386, `V7 -> ("i386/debian", tag)
-      | `Aarch32, (`V7 | `V8) -> ("arm32v7/debian", tag)
       | `Ppc64le, (`V8 | `V9 | `V10) -> ("ppc64le/debian", tag)
       | `S390x, (`V8 | `V9 | `V10) -> ("s390x/debian", tag)
       | _ -> ("debian", tag))
