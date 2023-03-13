ocurrent/ocaml-dockerfile

commit 58702a878fb5225aab961d119c7f6ccc21a82a0e

Reorder yum install to avoid superfluous yum update

src-opam/opam.ml
@@ -350,14 +350,15 @@ let yum_opam2 ?(labels = []) ?arch ~yum_workaround ~enable_powertools
   header ?arch distro
   @@ label (("distro_style", "rpm") :: labels)
   @@ run "yum --version || dnf install -y yum"
-  @@ workaround @@ Linux.RPM.update
-  @@ Linux.RPM.dev_packages ~extra:"which tar curl xz libcap-devel openssl" ()
+  @@ workaround
+  @@ Linux.RPM.install "which tar curl xz libcap-devel openssl sudo"
+  @@ Linux.RPM.groupinstall {|"Development Tools"|}
   @@ Linux.Git.init ()
   @@ maybe_build_bubblewrap_from_source distro
   @@ install_opams ~prefix:"/usr" opam_master_hash opam_branches
   @@ from ?arch distro
   @@ run "yum --version || dnf install -y yum"
-  @@ workaround @@ Linux.RPM.update
+  @@ workaround
   @@ bubblewrap_and_dev_packages distro
   @@ copy_opams ~src:"/usr/bin" ~dst:"/usr/bin" opam_branches
   @@ (if enable_powertools then

src-opam/linux.ml
@@ -32,10 +32,12 @@ let sudo_nopasswd = "ALL=(ALL:ALL) NOPASSWD:ALL"
 (** RPM rules *)
 module RPM = struct
   let update = run "yum update -y"
-  let install fmt = ksprintf (run "yum install -y %s && yum clean all") fmt
+
+  let install fmt =
+    ksprintf (fun s -> update @@ run "yum install -y %s && yum clean all" s) fmt
 
   let groupinstall fmt =
-    ksprintf (run "yum groupinstall -y %s && yum clean all") fmt
+    ksprintf (fun s -> run "yum groupinstall -y %s && yum clean all" s) fmt
 
   let add_user ?uid ?gid ?(sudo = false) username =
     let uid = match uid with Some u -> sprintf "-u %d " u | None -> "" in
@@ -59,11 +61,11 @@ module RPM = struct
     @@ workdir "%s" home @@ run "mkdir .ssh" @@ run "chmod 700 .ssh"
 
   let dev_packages ?extra () =
-    groupinstall "\"Development Tools\""
-    @@ install
-         "sudo passwd bzip2 patch rsync nano gcc-c++ git tar curl xz \
-          libX11-devel which m4 diffutils findutils%s"
-         (match extra with None -> "" | Some x -> " " ^ x)
+    install
+      "sudo passwd bzip2 patch rsync nano gcc-c++ git tar curl xz libX11-devel \
+       which m4 diffutils findutils%s"
+      (match extra with None -> "" | Some x -> " " ^ x)
+    @@ groupinstall "\"Development Tools\""
 
   let install_system_ocaml = install "ocaml ocaml-camlp4-devel ocaml-ocamldoc"
 end

commit 9a14969331a791dc3c75691c74791ebd70394d4a

Install bzip2 to build opam under yum
Problem introduced in 58702a878fb5225aab961d119c7f6ccc21a82a0e.

src-opam/opam.ml
@@ -351,7 +351,7 @@ let yum_opam2 ?(labels = []) ?arch ~yum_workaround ~enable_powertools
   @@ label (("distro_style", "rpm") :: labels)
   @@ run "yum --version || dnf install -y yum"
   @@ workaround
-  @@ Linux.RPM.install "which tar curl xz libcap-devel openssl sudo"
+  @@ Linux.RPM.install "which tar curl xz libcap-devel openssl sudo bzip2"
   @@ Linux.RPM.groupinstall {|"Development Tools"|}
   @@ Linux.Git.init ()
   @@ maybe_build_bubblewrap_from_source distro