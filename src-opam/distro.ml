80f16e21a38bca767d7945a28aa6737267a27aa2
Antonin Décimo <antonin@tarides.com>
2024-04-23 11:08:37 -0300

    Update to bubblewrap 0.9.0

--- src-opam/distro.ml
+++ src-opam/distro.ml
@@ -1343,7 +1343,7 @@ let bubblewrap_version (t : t) =
   | `Debian `V11 -> Some (0, 4, 1)
   | `Debian `V12 -> Some (0, 8, 0)
   | `Debian `Testing -> Some (0, 8, 0)
-  | `Debian `Unstable -> Some (0, 8, 0)
+  | `Debian `Unstable -> Some (0, 9, 0)
   | `CentOS `V6 -> None
   | `CentOS `V7 -> None
   | `CentOS `V8 -> Some (0, 4, 0)
@@ -1387,7 +1387,7 @@ let bubblewrap_version (t : t) =
   | `Alpine `V3_17 -> Some (0, 7, 0)
   | `Alpine `V3_18 -> Some (0, 8, 0)
   | `Alpine `V3_19 -> Some (0, 8, 0)
-  | `Archlinux `Latest -> Some (0, 8, 0)
+  | `Archlinux `Latest -> Some (0, 9, 0)
   | `OpenSUSE `V42_1 -> None (* Not actually checked *)
   | `OpenSUSE `V42_2 -> None (* Not actually checked *)
   | `OpenSUSE `V42_3 -> None (* Not actually checked *)
@@ -1397,7 +1397,7 @@ let bubblewrap_version (t : t) =
   | `OpenSUSE `V15_3 -> Some (0, 4, 1)
   | `OpenSUSE `V15_4 -> Some (0, 4, 1)
   | `OpenSUSE `V15_5 -> Some (0, 7, 0)
-  | `OpenSUSE `Tumbleweed -> Some (0, 8, 0)
+  | `OpenSUSE `Tumbleweed -> Some (0, 9, 0)
   | `Cygwin _ -> None
   | `Windows _ -> None
 
